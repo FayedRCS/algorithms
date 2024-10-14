521645b4e213c9431aa02fe6945798086c3453dd
fayed <fayed_rana@outlook.com>
2024-10-14 21:10:43 +0200

    challenge solution

--- chapter_1/median/chc2/main.py
+++ chapter_1/median/chc2/main.py
@@ -1,3 +1,19 @@
 def median_followers(nums):
-    pass
+    sorted_list = sorted(nums)
 
+    if nums == []:
+        return None
+    
+    median_index = len(sorted_list) / 2
+    
+    median = 0
+
+    if median_index % 2 == 1:
+        median_index = int(median_index)
+        median = (sorted_list[median_index] + sorted_list[median_index - 1]) / 2
+        return median
+    else:
+        median_index = int(median_index)
+        median = sorted_list[median_index]
+        return median
+    
