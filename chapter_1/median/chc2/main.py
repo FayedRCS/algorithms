80a3551c90d480f585f0a3a3b984414f2538d2b9
fayed <fayed_rana@outlook.com>
2024-10-17 15:22:12 +0200

    comments added

--- chapter_1/median/chc2/main.py
+++ chapter_1/median/chc2/main.py
@@ -1,13 +1,14 @@
 def median_followers(nums):
     sorted_list = sorted(nums)
 
+#return None if the list is empty
     if nums == []:
         return None
-    
+#finding the center of the list
     median_index = len(sorted_list) / 2
     
     median = 0
-
+#if the list doesnt have a perfect center, we return average of the two closest elements
     if median_index % 2 == 1:
         median_index = int(median_index)
         median = (sorted_list[median_index] + sorted_list[median_index - 1]) / 2
