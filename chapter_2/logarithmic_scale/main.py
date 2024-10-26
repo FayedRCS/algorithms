1914744cf265755c8e0f202af9ab9c8d8395a767
fayed <fayed_rana@outlook.com>
2024-10-26 16:44:09 +0200

    solution

--- chapter_2/logarithmic_scale/main.py
+++ chapter_2/logarithmic_scale/main.py
@@ -1,2 +1,18 @@
+import math
+
 def log_scale(data, base):
-    pass
+    #assumes there are only valid values in data, hence no exception handling   
+    
+    logarithms = []
+    
+    #iterate throught the data in list, return log of data with base number
+    for i in data:
+        log = math.log(i, base)
+        logarithms.append(log)
+    
+    return logarithms
+
+
+
+
+
