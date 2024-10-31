0161b3d50981d2992b124d806773ecded604b40b
fayed <fayed_rana@outlook.com>
2024-10-31 01:57:56 +0100

    solved

--- chapter_3/remove_duplicate_elements/main.py
+++ chapter_3/remove_duplicate_elements/main.py
@@ -1,4 +1,10 @@
 def remove_duplicates(nums):
-    pass
+    
+    no_dupes = []
 
-#
+    #appending to the new list if the element is not contained already
+    for i in nums:
+        if i not in no_dupes:
+            no_dupes.append(i)
+    
+    return no_dupes
