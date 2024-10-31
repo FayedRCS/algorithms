7ea9de1189c3d0c929b2665883c7a83df687eca8
fayed <fayed_rana@outlook.com>
2024-10-31 01:35:34 +0100

    solution added

--- chapter_3/name_count/main.py
+++ chapter_3/name_count/main.py
@@ -1,2 +1,13 @@
 def count_names(list_of_lists, target_name):
-    pass
+    
+    count = 0
+
+    for i in list_of_lists:
+        #iterates though each element in the nested list and counts each occurance of the target name
+        for j in i:
+            if target_name in j:
+                count += 1
+    
+    return count
+
+ #time complexity is O(n), but O(mn) if we consider m to be the number of lists and n the average lenght of a list
