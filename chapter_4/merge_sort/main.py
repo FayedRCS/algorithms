70f7b50804287ed33ab6bc7f56e5db71717f7020
fayed <fayed_rana@outlook.com>
2024-11-09 00:32:15 +0100

    merge sort solution w/ comments

--- chapter_4/merge_sort/main.py
+++ chapter_4/merge_sort/main.py
@@ -3,9 +3,39 @@ def merge_sort(nums):
     if len(nums) < 2:
         return nums
     
-    
+    #finding a int value for the list median
+    half = len(nums) // 2
 
+    #splitting list into two "slices" then we use recursion to repeat the process --> we're left with each element in it's own list
+    left_side = nums[:half]
+    right_side = nums[half:]
+
+    #recursion bit
+    leftie_sort = merge_sort(left_side)
+    rightie_sort = merge_sort(right_side)
+
+    return merge(leftie_sort, rightie_sort)
 
 
 def merge(first, second):
-    pass
+    
+    final = []
+
+    i = 0
+    j = 0
+
+    #"merging" both lists, incrementing our starter variables i,j respectively 
+    while i < len(first) and j < len(second):
+        if first[i] <= second[j]:
+            final.append(first[i])
+            i += 1
+        else:
+            final.append(second[j])
+            j += 1
+            
+    #adding, or extending rather the final with the rememnants of list
+    final.extend(first[i:])
+    final.extend(second[j:])
+    
+
+    return final
